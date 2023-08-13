9249c43f17cd528fa00b41af2dd58d234ba20a88
Jairo Andrés Portela Cortés <jairo.portela@imagineapps.co>
2023-08-13 13:14:59 -0500

    [Feature] add help command

--- src/commands/help_command.js
+++ src/commands/help_command.js
@@ -1,8 +1,15 @@
+import {
+  CreateExpenseCommandText,
+  CreateIncomeCommandText,
+} from '../constants/constants.js';
+
+CreateExpenseCommandText;
 const helpCommand = (ctx) => {
   const helpMessage = `
     Bienvenido al bot de finanzas. Aquí tienes una lista de comandos disponibles:
       
-    /crear_gasto - Crea un nuevo gasto.
+    /${CreateExpenseCommandText} - Crea un nuevo gasto.
+    /${CreateIncomeCommandText} - Crea un nuevo ingreso.
     /resumen_mes - Muestra un resumen financiero del mes actual.
       
     ¡Espero que esta información te sea útil!
